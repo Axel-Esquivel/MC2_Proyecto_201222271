66cbcf8894c10a6d76fb54944658ea7579d1d768
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-04-01 23:01:38 -0600

    proyecto finalizado

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var lista_cartas;
 var grupo_seleccion = 0;
-var carta_seleccionada = false;
+var numero_lanzamientos = 0;
+var carta_encontrada = false;
 
 function iniciar() {
     for (let k = 0; k < 20; k++) {
@@ -19,9 +20,19 @@ function iniciar() {
     agregar_cartas = document.getElementById('agregar-cartas');
     agregar_cartas.addEventListener('click', iniciar_movimientos);
 
-    iniciar_lanzamientos = document.getElementById('iniciar-lanzamientos');
-    iniciar_lanzamientos.addEventListener('click', realizar_lanzamientos)
+    iniciar_lanzamientos = document.getElementById('iniciar-lanzamiento');
+    iniciar_lanzamientos.addEventListener('click', realizar_lanzamiento)
+
+    grupo_1 = document.getElementById('grupo-1').addEventListener('click', establecer_grupo_1)
+    grupo_2 = document.getElementById('grupo-2').addEventListener('click', establecer_grupo_2)
+    grupo_3 = document.getElementById('grupo-3').addEventListener('click', establecer_grupo_3)
 }
+
+function establecer_grupo_1() { grupo_seleccion = 1; }
+
+function establecer_grupo_2() { grupo_seleccion = 2; }
+
+function establecer_grupo_3() { grupo_seleccion = 3; }
 
 function getN(k) {
     return 3 * (2 * k + 1);
@@ -33,26 +44,29 @@ function log(base, x) {
 
 function iniciar_movimientos() {
     lista_cartas = [];
-    carta_seleccionada = false;
+    carta_encontrada = false;
     numero_cartas = document.getElementById('numero_cartas').value;
 
     if (numero_cartas >= 9) {
         lista_cartas = crear_cartas(numero_cartas);
         repartir_cartas(lista_cartas);
     }
+
+    numero_lanzamientos = Math.ceil(log(3, lista_cartas.length));
 }
 
 function crear_cartas(numero_cartas) {
     var cartas_creadas = []
 
     for (let i = 0; i < numero_cartas; i++) {
-        const div = document.createElement('div')
+        const div = document.createElement('div');
 
-        div.id = i
-        div.className = "carta animate__backInUp"
-        div.addEventListener('click', seleccionar_carta)
+        div.id = i;
+        div.className = "carta animate__animated animate__backInUp";
+        div.addEventListener('click', seleccionar_carta);
+        div.innerText = i + 1
 
-        cartas_creadas.push(new Carta(i + 1, div))
+        cartas_creadas.push(new Carta(i + 1, div));
     }
 
     return cartas_creadas
@@ -61,39 +75,64 @@ function crear_cartas(numero_cartas) {
 function seleccionar_carta(e) {
     const carta = e.target;
 
-    if (carta_seleccionada == false) {
-        carta_seleccionada = true;
+    if (lista_cartas[carta.id].seleccionada == false) {
         carta.className = "carta seleccionada";
         lista_cartas[carta.id].seleccionada = true;
-        grupo_seleccion = lista_cartas[carta.id].grupo
     } else {
         if (lista_cartas[carta.id].seleccionada == true) {
-            grupo_seleccion = 0
             carta.className = "carta";
-            carta_seleccionada = false;
             lista_cartas[carta.id].seleccionada = false;
         }
     }
 }
 
-async function realizar_lanzamientos() {
-    if (carta_seleccionada == true && grupo_seleccion != 0) {
-        numero_lanzamientos = Math.ceil(log(3, lista_cartas.length));
+async function realizar_lanzamiento() {
+    if (numero_lanzamientos != 0 && grupo_seleccion != 0) {
+        var lista_a = [];
+        var lista_b = [];
+        var lista_seleccion = lista_cartas.filter(carta => carta.grupo == grupo_seleccion);
 
-        for (let lanzamiento = 1; lanzamiento <= numero_lanzamientos; lanzamiento++) {
-            console.log(`Lanzamiento ${ lanzamiento } grupo selección ${ grupo_seleccion }`)
-
-
-
-            await sleep(5000)
+        if (grupo_seleccion != 1 && grupo_seleccion != 2) {
+            lista_a = lista_cartas.filter(carta => carta.grupo == 1);
+            lista_b = lista_cartas.filter(carta => carta.grupo == 2);
         }
 
+        if (grupo_seleccion != 2 && grupo_seleccion != 3) {
+            lista_a = lista_cartas.filter(carta => carta.grupo == 2);
+            lista_b = lista_cartas.filter(carta => carta.grupo == 3);
+        }
+
+        if (grupo_seleccion != 3 && grupo_seleccion != 1) {
+            lista_a = lista_cartas.filter(carta => carta.grupo == 3);
+            lista_b = lista_cartas.filter(carta => carta.grupo == 1);
+        }
+
+        lista_cartas = []
+
+        lista_cartas = lista_cartas.concat(lista_a);
+        lista_cartas = lista_cartas.concat(lista_seleccion);
+        lista_cartas = lista_cartas.concat(lista_b);
+
+        lista_cartas.forEach(carta => carta.grupo = 0);
+
+        repartir_cartas(lista_cartas);
+
+        grupo_seleccion = 0;
+        numero_lanzamientos--;
+        carta_encontrada = true;
     } else {
-        alert('Debe seleccionar una carta, antes de inciiar los lanzamientos.')
+        alert('Debe el grupo en donde se encuentre la carta que selecciono o ya no tiene mas lanzamientos.')
+    }
+
+    if (numero_lanzamientos == 0 && carta_encontrada == true) {
+        posicion = (lista_cartas.length - 1) / 2;
+        carta = lista_cartas[posicion];
+
+        alert(`La carta que eligió fue: ${ carta.n }`);
     }
 }
 
-function repartir_cartas(cartas) {
+async function repartir_cartas(cartas) {
     contenedor_a = document.getElementById('contenedor-a');
     contenedor_b = document.getElementById('contenedor-b');
     contenedor_c = document.getElementById('contenedor-c');
@@ -103,10 +142,13 @@ function repartir_cartas(cartas) {
     contenedor_c.innerHTML = '';
 
     grupo = 1;
-    margin_1 = 0;
-    margin_2 = 0;
-    margin_3 = 0;
-    cartas.forEach(carta => {
+    margin_1 = 1;
+    margin_2 = 1;
+    margin_3 = 1;
+
+    for (let i = 0; i < lista_cartas.length; i++) {
+        const carta = lista_cartas[i];
+
         switch (grupo) {
             case 1:
                 carta.grupo = grupo;
@@ -119,7 +161,7 @@ function repartir_cartas(cartas) {
             case 2:
                 carta.grupo = grupo;
                 carta.contenedor = contenedor_b;
-                carta.carta.style.margin = `${margin_1}em ${margin_1}em`;
+                carta.carta.style.margin = `${margin_2}em ${margin_2}em`;
                 carta.contenedor.appendChild(carta.carta);
                 grupo = 3;
                 margin_2++;
@@ -127,15 +169,15 @@ function repartir_cartas(cartas) {
             case 3:
                 carta.grupo = grupo
                 carta.contenedor = contenedor_c
-                carta.carta.style.margin = `${margin_1}em ${margin_1}em`;
+                carta.carta.style.margin = `${margin_3}em ${margin_3}em`;
                 carta.contenedor.appendChild(carta.carta);
                 grupo = 1;
                 margin_3++;
                 break;
         }
 
-
-    });
+        await sleep(100)
+    }
 }
 
 function Carta(n, carta) {
@@ -147,7 +189,7 @@ function Carta(n, carta) {
 }
 
 function sleep(n) {
-    return new Promise(resolve=>setTimeout(resolve,n));
+    return new Promise(resolve => setTimeout(resolve, n));
 }
 
 iniciar()
